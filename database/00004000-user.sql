7992b313a83b70d877722cf05be3cf94fc9cb7dc
ali <ali@socienta.com>
2022-05-05 21:40:27 +0400

    Nested function corrected

--- database/00004000-user.sql
+++ database/00004000-user.sql
@@ -13,10 +13,7 @@ CREATE UNIQUE INDEX IF NOT EXISTS user_email_unique
     ON public."user" USING btree
     (lower(email) COLLATE pg_catalog."default" ASC NULLS LAST);
 
-CREATE OR REPLACE FUNCTION email_lower() RETURNS trigger AS $$ 
-BEGIN 
-	NEW.email := lower(NEW.email); RETURN NEW; END;
-$$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION email_lower() RETURNS trigger AS $inner$ BEGIN NEW.email := lower(NEW.email); RETURN NEW; END;$inner$ LANGUAGE plpgsql;
 CREATE OR REPLACE TRIGGER trigger_user_email_lower BEFORE INSERT OR UPDATE ON public."user" FOR EACH ROW EXECUTE PROCEDURE user_email_lower();
 
 SET client_min_messages TO NOTICE;
