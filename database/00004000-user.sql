b304b2d2e722970c27c8cb2e72758251868d05ff
ali <ali@socienta.com>
2022-05-05 21:47:28 +0400

    trigger transaction

--- database/00004000-user.sql
+++ database/00004000-user.sql
@@ -14,7 +14,12 @@ CREATE UNIQUE INDEX IF NOT EXISTS user_email_unique
     (lower(email) COLLATE pg_catalog."default" ASC NULLS LAST);
 
 CREATE OR REPLACE FUNCTION email_lower() RETURNS trigger AS $inner$ BEGIN NEW.email := lower(NEW.email); RETURN NEW; END;$inner$ LANGUAGE plpgsql;
-CREATE OR REPLACE TRIGGER trigger_user_email_lower BEFORE INSERT OR UPDATE ON public."user" FOR EACH ROW EXECUTE PROCEDURE user_email_lower();
+
+DO $trigger$
+	BEGIN
+		DROP TRIGGER IF EXISTS trigger_user_email_lower ON public."user";
+		CREATE OR REPLACE TRIGGER trigger_user_email_lower BEFORE INSERT OR UPDATE ON public."user" FOR EACH ROW EXECUTE PROCEDURE user_email_lower();
+	END; $trigger$ LANGUAGE plpgsql;
 
 SET client_min_messages TO NOTICE;
 
