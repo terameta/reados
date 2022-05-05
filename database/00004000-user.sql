aa39b17b49ff2fe842fe61481866e4b6d0f33c79
ali <ali@socienta.com>
2022-05-05 21:55:19 +0400

    Wrong function name fixed

--- database/00004000-user.sql
+++ database/00004000-user.sql
@@ -15,11 +15,10 @@ CREATE UNIQUE INDEX IF NOT EXISTS user_email_unique
 
 CREATE OR REPLACE FUNCTION email_lower() RETURNS trigger AS $inner$ BEGIN NEW.email := lower(NEW.email); RETURN NEW; END;$inner$ LANGUAGE plpgsql;
 
-DO $trigger$
-	BEGIN
-		DROP TRIGGER IF EXISTS trigger_user_email_lower ON public."user";
-		CREATE TRIGGER trigger_user_email_lower BEFORE INSERT OR UPDATE ON public."user" FOR EACH ROW EXECUTE PROCEDURE user_email_lower();
-	END; $trigger$ LANGUAGE plpgsql;
+DO $trigger$ BEGIN
+	DROP TRIGGER IF EXISTS trigger_user_email_lower ON public."user";
+	CREATE TRIGGER trigger_user_email_lower BEFORE INSERT OR UPDATE ON public."user" FOR EACH ROW EXECUTE PROCEDURE email_lower();
+END; $trigger$ LANGUAGE plpgsql;
 
 SET client_min_messages TO NOTICE;
 
